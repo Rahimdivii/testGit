0feb53f4978949b805f26f408e04fd55b51d01ac
Rahimdivii <106316919+Rahimdivii@users.noreply.github.com>
2023-07-13 18:54:23 +0530

    Initial Commit

---
+++ testGit/testGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitApp.swift
+//  testGit
+//
+//  Created by hanriver on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct testGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
